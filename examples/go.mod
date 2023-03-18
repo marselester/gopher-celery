503243b47c1cfceaabd05ca1e7361804ab59ca70
marselester <marselester@gmail.com>
2023-03-18 11:04:39 -0400

    Add goredis example

--- examples/go.mod
+++ examples/go.mod
@@ -11,19 +11,21 @@ require (
 	github.com/marselester/gopher-celery v0.0.0-00010101000000-000000000000
 	github.com/oklog/run v1.1.0
 	github.com/prometheus/client_golang v1.12.2
+	github.com/redis/go-redis/v9 v9.0.2
 )
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
-	github.com/go-logfmt/logfmt v0.5.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.35.0 // indirect
 	github.com/prometheus/procfs v0.7.3 // indirect
-	golang.org/x/sync v0.0.0-20220923202941-7f9b1623fab7 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.0.0-20220704084225-05e143d24a9e // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 )
---
+++ examples/goredis/main.go
@@ -0,0 +1,57 @@
+// Program goredis shows how to use github.com/redis/go-redis.
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
+	celery "github.com/marselester/gopher-celery"
+	celeryredis "github.com/marselester/gopher-celery/goredis"
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	logger := log.NewJSONLogger(log.NewSyncWriter(os.Stderr))
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	c := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	defer func() {
+		if err := c.Close(); err != nil {
+			level.Error(logger).Log("msg", "failed to close Redis client", "err", err)
+		}
+	}()
+
+	if _, err := c.Ping(ctx).Result(); err != nil {
+		level.Error(logger).Log("msg", "Redis connection failed", "err", err)
+		return
+	}
+
+	broker := celeryredis.NewBroker(
+		celeryredis.WithClient(c),
+	)
+	app := celery.NewApp(
+		celery.WithBroker(broker),
+		celery.WithLogger(logger),
+	)
+	app.Register(
+		"myproject.mytask",
+		"important",
+		func(ctx context.Context, p *celery.TaskParam) error {
+			p.NameArgs("a", "b")
+			fmt.Printf("received a=%s b=%s\n", p.MustString("a"), p.MustString("b"))
+			return nil
+		},
+	)
+
+	logger.Log("msg", "waiting for tasks...")
+	err := app.Run(ctx)
+	logger.Log("msg", "program stopped", "err", err)
+}
