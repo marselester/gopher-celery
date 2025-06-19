8564f92af631c10948e4b0aedfdffc3245707314
Ron Cemer <ron.cemer@lancium.com>
2025-06-19 09:44:43 -0400

    Add RabbitMQ consumer example.

---
+++ examples/rabbitmq-consumer/main.go
@@ -0,0 +1,39 @@
+// Program consumer receives "myproject.mytask" tasks from "celery" queue.
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/go-kit/log"
+	celery "github.com/roncemer/gopher-celery-with-rabbitmq-broker"
+    celeryrabbitmq "github.com/roncemer/gopher-celery-with-rabbitmq-broker/rabbitmq"
+)
+
+func main() {
+	broker := celeryrabbitmq.NewBroker(celeryrabbitmq.WithAmqpUri("amqp://guest:guest@localhost:5672/"))
+	logger := log.NewJSONLogger(log.NewSyncWriter(os.Stderr))
+
+	app := celery.NewApp(
+        celery.WithBroker(broker),
+		celery.WithLogger(logger),
+	)
+	app.Register(
+		"myproject.mytask",
+		"celery",
+		func(ctx context.Context, p *celery.TaskParam) error {
+			p.NameArgs("a", "b")
+			fmt.Printf("received a=%s b=%s\n", p.MustString("a"), p.MustString("b"))
+			return nil
+		},
+	)
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	logger.Log("msg", "waiting for tasks...")
+	err := app.Run(ctx)
+	logger.Log("msg", "program stopped", "err", err)
+}
